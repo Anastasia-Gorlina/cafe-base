21b12da6ebcf9407642d9719fac2de20165ed34a
Anastasia Gorlina <gobi199@yandex.ru>
2023-07-17 19:11:29 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app"><Profile /><Cafe /></div>
+  </template>
+
+<script>
+import Profile from "./components/Profile.vue";
+import Cafe from "./components/Beer.vue";
+
+export default {
+  name: "App",
+  components: { Profile, Cafe },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 60px auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 50px;
+}
+
+.info > div:not {
+  margin-bottom: 0.6em;
+}
+</style>
---
+++ src/components/Cafe.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="Cafe">
+    <div v-if="!loading" class="info">
+      <div>Name: {{ name }}</div>
+      <div>Address: {{ address }}</div>
+      <div>Landmark: {{ landmark }}</div>
+      <div>Cuisine: {{ cuisine }}</div>
+      <div>Distance: {{ distance }}</div>
+      <div>Time: {{ time }}</div>
+      <div>Photo: {{ photo }}</div>
+      <div>Time: {{ time }}</div>
+      <div>Photo: {{ photo }}</div>
+      <div>Business_lunch: {{ business_lunch }}</div>
+      <div>Price: {{ price }}</div>
+    </div>
+    <div v-else>Loading...</div>
+    <button @click="displayData()" class="cafe-base">---</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cafe",
+
+  data() {
+    return {
+      name: true,
+      address: null,
+      landmark: null,
+      cuisine: null,
+      distance: null,
+      time: null,
+      photo: null,
+      buisness_lunch: null,
+      price: null,
+    };
+  },
+
+  methods: {
+    async getRandomBeer() {
+    this.name = true;
+
+    return fetch('<https://bandaumnikov.ru/api/test/site/get-index>').then(
+        (response) => response.json()
+      );
+    },
+    displayData() {
+      this.getRandomBeer().then((cafeData) => {
+        this.name = cafeData.name;
+        this.address = cafeData.address;
+        this.landmark = cafeData.landmark;
+        this.cuisine = cafeData.cuisine;
+        this.distance = cafeData.distance;
+        this.time = cafeData.time;
+        this.photo = cafeData.photo;
+        this.buisness_lunch = cafeData.buisness_lunch;
+        this.price = cafeData.price;
+
+        this.loading = false;
+      });
+    },
+  },
+
+  created() {
+    this.displayData();
+  },
+};
+</script>
+
+<style>
+.cafe {
+  width: 300px;
+}
+
+.cafe-base {
+  margin-top: 20px;
+  width: 100px;
+  height: 30px;
+  background-color: white;
+  border: 1px solid black;
+}
+</style>
